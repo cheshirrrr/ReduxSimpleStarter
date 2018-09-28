b992ff1b566ca394d625943538420f125cc911f4
dkirhmeier <dkirhmeier@asset-control.com>
2018-09-28 16:34:33 +0200

    removed api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyAK5BG6YjfjoUXQEVC5M8SDlJcqnW_mBww';
-
 const App = () => <div><SearchBar/></div>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));
